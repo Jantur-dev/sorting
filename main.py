1e2b1ca4f1d4c4ccbc82214fac47727d81a60b4b
Jantur-dev <jantur29@gmail.com>
2023-01-29 23:15:41 +0700

    Bogo Sort ?

---
+++ main.py
@@ -0,0 +1,21 @@
+from random import randint, shuffle
+def create_array(min=10, max=10):
+    return [randint(0, max) for arr in range(min)]
+def is_sort(l):
+    for i in range(1, len(x)):
+        if l[i] < l[i-1]:
+            return False
+    return True
+def bogo_sort(x):
+    jml = 0
+    while not is_sort(x):
+        shuffle(x)
+        jml+=1
+    return jml, x
+
+x = create_array(10, 10)
+y, z = bogo_sort(x)
+
+print(f"Sorted array : {z}")
+print(f"Number of Iterations : {y}")
+print(f"Unsorted array : {x}")
